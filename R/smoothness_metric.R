c7d0b8d25c8142db180467761b6bd5dc1ef60e95
Lukas Weber <lmweberedu@gmail.com>
2024-03-04 22:56:28 -0500

    more general inputs for evaluation metric function

--- R/smoothness_metric.R
+++ R/smoothness_metric.R
@@ -1,4 +1,4 @@
-#' Function for evaluation metric
+#' Function for smoothness metric
 #' 
 #' Function for clustering smoothness evaluation metric
 #' 
@@ -6,11 +6,12 @@
 #' number of nearest neighbors per point that are from a different cluster.
 #' 
 #' 
-#' @param input Input object, assumed to be a \code{SpatialExperiment} object
-#'   containing a column of cluster labels in `colData`.
+#' @param spatialcoords Numeric matrix containing spatial coordinates of points,
+#'   formatted as nrow = number of points, ncol = 2 (assuming x and y
+#'   dimensions). For example, `spcoords = spatialCoords(spe)`.
 #' 
-#' @param label Name of column in `colData` containing cluster labels. Default =
-#'   "label".
+#' @param labels Numeric vector of cluster labels for each point. For example,
+#'   `labels <- as.numeric(colData(spe)$label)`.
 #' 
 #' @param k Number of k nearest neighbors to use in calculation. Default = 6
 #'   (from 10x Genomics Visium platform).
@@ -19,27 +20,22 @@
 #' @return Returns value of evaluation metric.
 #' 
 #' 
-#' @importFrom SummarizedExperiment colData
 #' @importFrom spdep knearneigh
 #' 
 #' @export
 #' 
 #' @examples
-#' # Example shown in vignette.
+#' # Example to be shown in vignette.
 #' 
-smoothness_metric <- function(input, label = "label", k = 6) {
+smoothness_metric <- function(spatialcoords, labels, k = 6) {
   
-  spe <- input
-  stopifnot(is(spe, "SpatialExperiment"))
-  
-  # extract spatial coordinates for each point
-  spatialcoords <- spatialCoords(spe)
+  stopifnot(length(labels) == nrow(spatialcoords))
+  stopifnot(ncol(spatialcoords) != 2)
   
   # calculate k nearest neighbors for each point
   neigh <- knearneigh(spatialcoords, k = k)$nn
   
   # calculate ordered columns of cluster labels
-  labels <- as.numeric(colData(spe)$label)
   neigh_labels <- matrix(NA, nrow = nrow(neigh), ncol = ncol(neigh))
   for (i in seq_len(ncol(neigh_labels))) {
     neigh_labels[, i] <- labels[neigh[, i]]
