31d07a5c42416f93d305eb9c14ac92153161d115
Lukas Weber <lmweberedu@gmail.com>
2024-04-16 10:26:17 -0400

    remove test

--- tests/testthat/test_smoothness_metric.R
+++ tests/testthat/test_smoothness_metric.R
@@ -6,7 +6,3 @@ test_that("results object has correct structure", {
   expect_is(res, "list")
   expect_length(res, 2)
 })
-
-test_that("metric output values for example dataset are correct", {
-  expect_equal(sum(res$n_discordant), 2046)
-})
