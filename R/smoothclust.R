e7e93ae9b157778bba8d32ac23dd4d1b31f62592
Lukas Weber <weberlm3@gmail.com>
2026-04-26 17:04:56 -0400

    speed update - sparse aggregation for kernel weight sums

--- R/smoothclust.R
+++ R/smoothclust.R
@@ -185,9 +185,9 @@ smoothclust <- function(input, assay_name = "counts", spatial_coords = NULL,
     raw_weights <- exp(-dists / bandwidth_scaled)
     
     # 4. normalize weights so each column in W sums to 1
-    col_sums <- numeric(N)
-    col_sums_tmp <- tapply(raw_weights, j_idx, sum)
-    col_sums[as.integer(names(col_sums_tmp))] <- col_sums_tmp
+    col_sums <- as.numeric(sparseMatrix(i = j_idx, 
+                                        j = rep.int(1L, length(j_idx)), 
+                                        x = raw_weights, dims = c(N, 1)))
     x_val <- raw_weights / col_sums[j_idx]
     
   } else if (method == "knn") {
